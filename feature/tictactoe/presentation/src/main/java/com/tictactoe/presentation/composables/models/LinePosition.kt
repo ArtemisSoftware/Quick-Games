8ecd4e2dace2ff82e3ab1b4ba4e2893a0e5e9618
gustavo_dev <gustavo.isaac@gmail.com>
2023-05-19 19:14:35 +0100

    LinePosition

---
+++ feature/tictactoe/presentation/src/main/java/com/tictactoe/presentation/composables/models/LinePosition.kt
@@ -0,0 +1,8 @@
+package com.tictactoe.presentation.composables.models
+
+enum class LinePosition {
+
+    HORIZONTAL_TOP, HORIZONTAL_MIDDLE, HORIZONTAL_BOTTOM,
+    VERTICAL_LEFT, VERTICAL_MIDDLE, VERTICAL_RIGHT,
+    DIAGONAL_RIGHT, DIAGONAL_LEFT
+}
