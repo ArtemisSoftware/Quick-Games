ea28c0c74854b17656bf838173d52e326fbcd6da
gustavo_dev <gustavo.isaac@gmail.com>
2023-05-19 20:05:58 +0100

    TicTacToeScreen

---
+++ feature/tictactoe/presentation/src/main/java/com/tictactoe/presentation/TicTacToeScreen.kt
@@ -0,0 +1,62 @@
+package com.tictactoe.presentation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.tictactoe.presentation.composables.Board
+import com.tictactoe.presentation.composables.PlayerOptions
+import com.tictactoe.presentation.composables.ScoreBoard
+
+@Composable
+fun TicTacToeScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+//            .background(GrayBackground)
+            .padding(horizontal = 30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        ScoreBoard(modifier = Modifier.fillMaxWidth())
+        Text(
+            text = "Tic Tac Toe",
+            fontSize = 50.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Cursive,
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+//                .aspectRatio(1f)
+//                .shadow(
+//                    elevation = 10.dp,
+//                    shape = RoundedCornerShape(20.dp)
+//                )
+//                .clip(RoundedCornerShape(20.dp))
+//                .background(GrayBackground)
+            ,
+            contentAlignment = Alignment.Center
+        ) {
+            Board(boardSize = 300.dp)
+        }
+        PlayerOptions(modifier = Modifier.fillMaxWidth())
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun TicTacToeScreenPreview() {
+    TicTacToeScreen()
+}
