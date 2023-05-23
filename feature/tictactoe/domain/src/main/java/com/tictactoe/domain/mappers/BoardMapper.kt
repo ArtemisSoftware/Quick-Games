5c36af994810ec771be3693aeb70d98b8675ceb4
gustavo_dev <gustavo.isaac@gmail.com>
2023-05-23 11:42:34 +0100

    ai - BoardMapper

---
+++ feature/tictactoe/domain/src/main/java/com/tictactoe/domain/mappers/BoardMapper.kt
@@ -0,0 +1,35 @@
+package com.tictactoe.domain.mappers
+
+import com.tictactoe.domain.models.MoveType
+import com.tictactoe.domain.models.ai.Board
+import com.tictactoe.domain.models.ai.Cell
+import com.tictactoe.domain.models.ai.CellState
+
+fun Array<Array<MoveType?>>.toBoard(): Board{
+
+    val board: MutableMap<Cell, CellState> = mutableMapOf()
+
+    this.forEachIndexed { y, _ ->
+        this[y].forEachIndexed { x, move ->
+
+            move?.let { moves ->
+                Cell.getCell(y = y, x = x)?.let { cell ->
+                    board[cell] = moves.toCellState()
+                }
+            }
+
+        }
+    }
+
+    return Board(
+        board = board
+    )
+
+}
+
+fun MoveType.toCellState(): CellState{
+    return when(this){
+        MoveType.X -> CellState.Star
+        MoveType.O -> CellState.Circle
+    }
+}
