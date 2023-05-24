67a64ad43da7acb7ae50fa4e44983847360001a7
gustavo_dev <gustavo.isaac@gmail.com>
2023-05-24 16:39:49 +0100

    SnakeScreen

---
+++ feature/snake/presentation/src/main/java/com/snake/presentation/SnakeScreen.kt
@@ -0,0 +1,38 @@
+package com.snake.presentation
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.tooling.preview.Preview
+import com.snake.presentation.composables.Board
+import com.snake.presentation.composables.GamePad
+
+@Composable
+fun SnakeScreen(
+    state: SnakeState,
+    event: (SnakeEvents) ->Unit,
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Board(state = state)
+
+        GamePad(
+            onDirectionChange = {
+                event.invoke(SnakeEvents.MoveSnake(it))
+            },
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun BoardPreview() {
+    SnakeScreen(
+        state = SnakeState(
+            food = Pair(5, 5),
+            snake = listOf(Pair(7, 7)),
+        ),
+        event = {},
+    )
+}
